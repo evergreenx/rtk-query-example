39b2bb3a7b3a257a76602dd04fffaef76e7d85aa
ido evergeen <dicksonevegreenido@gmail.com>
2023-08-06 20:59:17 +0100

    add dummy screens

--- app/index.js
+++ app/index.js
@@ -5,31 +5,90 @@ import {
   View,
   TextInput,
   TouchableOpacity,
+  Image,
 } from "react-native";
 import SearchComponent from "../components/inputs";
+
+import { FlashList } from "@shopify/flash-list";
 import React from "react";
 import ProductsCard from "../components/productsCard";
+import Svg, { Circle, Rect, Path } from "react-native-svg";
 
 export default function Page() {
   const [searchText, setSearchText] = React.useState("");
 
-  return (
-    <SafeAreaView className="flex-1 ">
-      <View className="flex-1 bg-white px-[20px] py-[20px]  ">
-        <SearchComponent />
+  const _renderItem = ({ item }) => {
+    return <ProductsCard />;
+  };
 
+  const DATA = [
+    {
+      title: "First Item",
+    },
+    {
+      title: "Second Item",
+    },
+
+    {
+      title: "First Item",
+    },
+    {
+      title: "Second Item",
+    },
+    {
+      title: "First Item",
+    },
+    {
+      title: "Second Item",
+    },
+  ];
+
+  const renderHeader = () => {
+    return (
+      <View>
         <Text className=" font-bold text-secondary text-[36px]">
           Find your suitable product now.
         </Text>
+      </View>
+    );
+  };
 
-        <View className="flex flex-row flex-wrap space-x-28">
-          {Array(10)
-            .fill()
-            .map((_, i) => (
-              <ProductsCard key={i} />
-            ))}
-          <ProductsCard />
+  return (
+    <SafeAreaView className="flex-1 bg-white pt-[60px] ">
+      <View className="flex-1 bg-white px-[20px] pt-[20px] ">
+        <View className="flex-row  justify-between mb-[20px] ">
+          <View className="flex-row self-center">
+            <Svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <Path
+                d="M1 12.3333H12.9014M1 5H23M1 19.6667H23"
+                stroke="#1B153D"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </Svg>
+          </View>
+
+          <SearchComponent />
         </View>
+        <FlashList
+          data={DATA}
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+          renderItem={({ item }) => _renderItem(item)}
+          estimatedItemSize={200}
+          contentContainerStyle={{
+            padding: 5,
+          }}
+          ListHeaderComponent={renderHeader}
+        />
       </View>
     </SafeAreaView>
   );
--- components/productsCard.js
+++ components/productsCard.js
@@ -1,20 +1,25 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 
 export default function ProductsCard() {
   return (
-    <View className="mt-[30px] w-[157px] justify-center ">
-      <Image
-        className="w-[141px] h-[101px] rounded-[10px]"
-        source={{
-          uri: "https://res.cloudinary.com/evergreenx/image/upload/v1691341750/unsplash_J4DnKxz_3sA_klnoqh.png",
-        }}
-      />
-      <Text className="text-secondary font-semibold text-base text-left mt-[16px] mb-[8px]">
-        Title
-      </Text>
+    <Link 
+    href="/details"
+    className="mt-[30px]  p-[8px] flex-1 justify-center ">
+      <View className="">
+        <Image
+          className="w-[141px] h-[101px] rounded-[10px]"
+          source={{
+            uri: "https://res.cloudinary.com/evergreenx/image/upload/v1691341750/unsplash_J4DnKxz_3sA_klnoqh.png",
+          }}
+        />
+        <Text className="text-secondary font-semibold text-base text-left mt-[16px] mb-[8px]">
+          Title
+        </Text>
 
-      <Text className="text-primary text-base font-medium">Price</Text>
-    </View>
+        <Text className="text-primary text-base font-medium">$Price</Text>
+      </View>
+    </Link>
   );
 }
